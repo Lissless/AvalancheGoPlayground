f23a4861139f43c57492f9356a52b46f352e539c
Alisalee Aaron <alisalee.aaron@gmail.com>
2022-03-29 13:46:50 -0400

    Added Fields to the CSV

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"math/big"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/ava-labs/avalanchego/api"
 	"github.com/ava-labs/avalanchego/ids"
@@ -98,10 +99,41 @@ func decodeContainer(height float64) (string, indexer.Container) {
 	return containerID, cont
 }
 
+func findAllValidBlockIDs(containerData []byte) string {
+	length := len(containerData)
+	if length < 32 {
+		fmt.Println("invalid length for possible BlockID")
+		return ""
+	}
+	var validBlockIDs []string
+	for i := 0; i < length-31; i++ {
+		check := containerData[i:(i + 32)]
+		stringID, err := formatting.EncodeWithChecksum(formatting.CB58, check)
+		if err != nil {
+			fmt.Println("fatal error")
+			break
+		}
+		time.Sleep((1 * time.Second))
+		fmt.Println("Processing possibility number ", (i + 1), " of ", (length - 31))
+		resp := getBlock(stringID)
+		if resp.Block != nil {
+			validBlockIDs = append(validBlockIDs, stringID)
+		}
+	}
+
+	valid := ""
+	for i := 0; i < len(validBlockIDs); i++ {
+		valid = valid + validBlockIDs[i] + ", "
+	}
+
+	return valid
+}
+
 func findBlockID(containerData []byte, wanted string) (int, int, string) {
 	length := len(containerData)
 	if length < 32 {
 		fmt.Println("invalid length for possible BlockID")
+		return 0, 0, ""
 	}
 	var blockIDs []string
 	found := -1
@@ -148,39 +180,124 @@ func getBlock(blockID string) api.GetBlockResponse {
 	return *out
 }
 
-func makeResearchCSV(blockID string, numEntries int) {
-	csvFile, err := os.Create("containerResearchOlder.csv")
+func makeBlockIDAtCSV(blockID string, numEntries int) {
+	csvFile, err := os.Create("containerResearchP58.csv")
 	defer csvFile.Close()
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
 	}
+
 	csvwriter := csv.NewWriter(csvFile)
 	defer csvwriter.Flush()
-	initialRow := []string{"Block ID", "Block Height", "Container ID", "Num of Possible IDs", "Index Appeared At", "Possible Transaction Type"}
+	initialRow := []string{"Block ID", "Block Height", "Container ID", "Num of Possible IDs", "Index Appeared At", "Possible Transaction Type", "Source or Destination Chain", "Chain Name", "Exported Outputs or Imported Inputs"}
 	if err := csvwriter.Write(initialRow); err != nil {
 		log.Fatalln("error writing record to file", err)
 	}
+
+	var chain string
+	var chainName string
+	var expinp string
+	for i := 0; i < numEntries; i++ {
+		chain = "N/A"
+		chainName = "N/A"
+		expinp = "N/A"
+		resp := getBlock(blockID)
+		block := resp.Block.(map[string]interface{})
+		if checkNotEmpty(block) {
+			chain, chainName = getChainType(block)
+			expinp = getExportOrImportType(block)
+		}
+
+		height := block["height"].(float64)
+		containerID, container := decodeContainer(height)
+		possibleIDs, appearedAt, transactionID := findBlockID(container.Bytes, blockID)
+		row := []string{blockID, fmt.Sprintf("%f", height), containerID, strconv.Itoa(possibleIDs), strconv.Itoa(appearedAt), transactionID, chain, chainName, expinp}
+
+		if err := csvwriter.Write(row); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+
+		blockID = block["parentID"].(string)
+	}
+}
+
+func checkNotEmpty(block map[string]interface{}) bool {
+	for k := range block {
+		if k == "tx" {
+			return true
+		}
+	}
+	return false
+}
+
+func getChainType(block map[string]interface{}) (string, string) {
+	tx := block["tx"].(map[string]interface{})
+	txUnsigned := tx["unsignedTx"].(map[string]interface{})
+	for k := range txUnsigned {
+		if k == "sourceChain" {
+			return "source", txUnsigned["sourceChain"].(string)
+		} else if k == "destinationChain" {
+			return "destination", txUnsigned["destinationChain"].(string)
+		}
+	}
+	return "N/A", "N/A"
+
+}
+
+func getExportOrImportType(block map[string]interface{}) string {
+	tx := block["tx"].(map[string]interface{})
+	txUnsigned := tx["unsignedTx"].(map[string]interface{})
+	for k := range txUnsigned {
+		if k == "exportedOutputs" {
+			return "Exported Outputs"
+		} else if k == "importedInputs" {
+			return "Imported Inputs"
+		}
+	}
+	return "N/A"
+}
+
+func makeFindRelevantIDsCSV(blockID string, numEntries int) {
+	csvFile, err := os.Create("containerValidIDsLarge.csv")
+	defer csvFile.Close()
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	csvwriter := csv.NewWriter(csvFile)
+	defer csvwriter.Flush()
+	initialRow := []string{"Block Height", "Container ID", "Found Valid Block IDs"}
+	if err := csvwriter.Write(initialRow); err != nil {
+		log.Fatalln("error writing record to file", err)
+	}
+
 	for i := 0; i < numEntries; i++ {
 		resp := getBlock(blockID)
 		block := resp.Block.(map[string]interface{})
 		height := block["height"].(float64)
 		containerID, container := decodeContainer(height)
-		possibleIDs, appearedAt, transactionID := findBlockID(container.Bytes, blockID)
-		row := []string{blockID, fmt.Sprintf("%f", height), containerID, strconv.Itoa(possibleIDs), strconv.Itoa(appearedAt), transactionID}
-
+		fmt.Println("Doing entry ", (i + 1), " of ", numEntries)
+		valid := findAllValidBlockIDs(container.Bytes)
+		row := []string{fmt.Sprintf("%f", height), containerID, valid}
 		if err := csvwriter.Write(row); err != nil {
 			log.Fatalln("error writing record to file", err)
 		}
+
 		blockID = block["parentID"].(string)
 	}
 
 }
 
 func main() {
-	dc := "11Atn7rC6LxAVZ3Dyc8xxB8Uz3LMxzos6TjbojRsav3zcJECB919hqakKzUPUTdTiynWUfpMsL2j1nEbcW4WWP1ZkwG48R6DBvUX4s6MVsCm9CdtpU1WbpwSBiPrMKEsZaZkuheaqJdMMtCeyZ8EyFYvJNLMtwyaS43LTb"
-	decodeUTXO(dc)
+	// dc := "11Atn7rC6LxAVZ3Dyc8xxB8Uz3LMxzos6TjbojRsav3zcJECB919hqakKzUPUTdTiynWUfpMsL2j1nEbcW4WWP1ZkwG48R6DBvUX4s6MVsCm9CdtpU1WbpwSBiPrMKEsZaZkuheaqJdMMtCeyZ8EyFYvJNLMtwyaS43LTb"
+	// decodeUTXO(dc)
 
-	//makeResearchCSV("26HeCVKx9viUPp5EdkitRMB2tkdzF62dNW1snx9sZGe461m4V2", 100)
-	// makeResearchCSV("pELkACstxyEW6pUiLJxBXhWNb3UadxW5MR7NZ47NM7NEbKJX3", 200)
+	// fmt.Println("start sleeping")
+	// time.Sleep((5 * time.Second))
+	// fmt.Println("Awoken")
+
+	// makeBlockIDAtCSV("26HeCVKx9viUPp5EdkitRMB2tkdzF62dNW1snx9sZGe461m4V2", 100)
+	// makeFindRelevantIDsCSV("2HRsYbDTxyxsE9FXgyw6ZtLSzRjNgdSCXxe85kpTwAcVenrv12", 1)
+	makeBlockIDAtCSV("GhZVPpopX84JrKiNNP2AXFwyaMBYbcnzG9TdP7Trf2bxVXgC7", 200)
 
 }
